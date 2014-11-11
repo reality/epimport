08046b30528cb12437bb189514373e6425d04ba9
reality <tinmachin3@gmail.com>
2014-11-11 17:03:16 +0000

    convert html

--- epimport.js
+++ epimport.js
@@ -3,13 +3,20 @@
 var _ = require('underscore')._,
     request = require('request'),
     fs = require('fs'),
-    async = require('async');
+    async = require('async'),
+    toMarkdown = require('to-markdown').toMarkdown,
+    html_strip = require('htmlstrip-native').html_strip;
 
 
 var config = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
+var htmlStripOptions = {
+    'include_script': false,
+    'include_style': false,
+    'compact_whitespace': false
+};
 
 // Check for missing config
-var diff = _.difference(_.keys(config), [ 'instance', 'api_key', 'file_dir', 'user_group' ]);
+var diff = _.difference(_.keys(config), [ 'instance', 'api_key', 'file_dir', 'user_group', 'convert_html' ]);
 if(diff.length !== 0) {
     console.log('Missing settings: ' + diff.join(', '));
     process.exit(1);
@@ -21,6 +28,9 @@ var files = fs.readdirSync(config.file_dir),
 
 async.each(files, function(file, done) {
     var content = fs.readFileSync(config.file_dir + '/' + file, 'utf-8');
+    if(config.convert_html) {
+        content = html_strip(toMarkdown(content), htmlStripOptions);
+    }
 
     request.get({
         'url': createPath,
